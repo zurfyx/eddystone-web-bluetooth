7ef5a9527623923ce99f336a7a4748893bbf4e02
zurfyx <zurfyx@gmail.com>
2017-06-25 16:24:11 +0200

    beacon lock status

--- src/beacon/BeaconService.ts
+++ src/beacon/BeaconService.ts
@@ -1,4 +1,24 @@
+import constants from '../constants';
+
+enum LOCK_VALUES {
+  'LOCKED' = 0x00,
+  'UNLOCKED' = 0x01,
+  'UNLOCKED AND AUTOMATIC RELOCK DISABLED' = 0x02,
+}
+
 export class BeaconService {
 
   constructor(public service: BluetoothRemoteGATTService) {}
+
+  private async readCharacteristic(uuid: string): Promise<DataView> {
+    const characteristic = await this.service.getCharacteristic(uuid);
+    return characteristic.readValue();
+  }
+
+  async isLocked() {
+    const lockUuid = constants.EDDYSTONE_LOCK_STATE_CHARACTERISTIC_UUID;
+    const rawVal = await this.readCharacteristic(lockUuid);
+    const val = rawVal.getInt8(0);
+    return val === LOCK_VALUES.LOCKED;
+  }
 }
