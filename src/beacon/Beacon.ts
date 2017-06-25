c4663b1d619150470e8fd2a52a03781303581d43
zurfyx <zurfyx@gmail.com>
2017-06-25 16:02:07 +0200

    bluetooth gatt connection

--- src/beacon/Beacon.ts
+++ src/beacon/Beacon.ts
@@ -1,4 +1,21 @@
+import constants from '../constants';
+import { BeaconService } from './BeaconService';
+
 export class Beacon {
 
   constructor(public device: BluetoothDevice) {}
+
+  onDisconnect(listener: (this: this, ev: Event) => any) {
+    this.device.addEventListener('gattserverdisconnected', listener);
+  }
+
+  async connect(): Promise<BeaconService> {
+    if (!this.device.gatt) {
+      return Promise.reject('Bluetooth device is probably not a beacon - it does not support GATT');
+    }
+    const bluetoothGattServer = await this.device.gatt.connect();
+    const service = await bluetoothGattServer
+      .getPrimaryService(constants.EDDYSTONE_CONFIG_SERVICE_UUID);
+    return new BeaconService(service);
+  }
 }
---
+++ src/beacon/BeaconService.ts
@@ -0,0 +1,4 @@
+export class BeaconService {
+
+  constructor(public service: BluetoothRemoteGATTService) {}
+}
